3367f20bb9c3843d46f0f0f24aed5ff1bb588901
ASgmbv <alimbeksagymbaev@gmail.com>
2020-10-28 17:51:45 +0600

    changed products

--- assets/data/data.js
+++ assets/data/data.js
@@ -7,7 +7,7 @@ export const sections = [
         description:
           "Two eggs any style served with choice of bacon or sausage, hasbrowns and OSM toast",
         price: 12.25,
-        image: "/pizza.png",
+        image: "/food/regular.png",
         type: "classics-regular",
       },
       {
@@ -15,7 +15,7 @@ export const sections = [
         description:
           "Two eggs any style with one OSM pancake or butter pancake  and your choice of bacon or sausage",
         price: 12.5,
-        image: "/abdurahman.jpg",
+        image: "/food/signature.png",
         type: "classics-signature",
       },
       {
@@ -23,7 +23,7 @@ export const sections = [
         description:
           "Our homemade buttermilk biscuits and hash browns smothered with pepper sausage country gravy",
         price: 10.5,
-        image: "/pizza.png",
+        image: "/food/gravy.png",
         type: "classics-biscuits-gravy",
       },
       {
@@ -31,7 +31,7 @@ export const sections = [
         price: 18.5,
         description:
           "Wyoming Gourmet Beef all natural grilled 6-ounce steak served with two eggs any style, hash browns OSM toast",
-        image: "/pizza.png",
+        image: "/food/steak.png",
         type: "classics-steak-eggs",
       },
     ],
