fb29fa2201565cea05ace123f525d5892f6c5a14
ASgmbv <alimbeksagymbaev@gmail.com>
2021-02-06 18:36:18 +0600

    changes

--- components/Header.js
+++ components/Header.js
@@ -20,6 +20,7 @@ import Link from "next/link";
 import NextImage from "next/image";
 import React from "react";
 import Bag from "../assets/icons/Bag";
+import Head from "next/head";
 
 const itemsSelector = (state) => state.items;
 
@@ -28,100 +29,120 @@ const Header = ({ restaurant, withCart = true }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <chakra.header
-      pos="fixed"
-      top="0"
-      left="0"
-      right="0"
-      zIndex="2"
-      width="full"
-      borderBottom="1px solid"
-      borderBottomColor="gray.100"
-      bg="white"
-      boxShadow="sm"
-    >
-      <Container
-        maxW="7xl"
-        height={["3rem", null, "4rem"]}
-        mx="auto"
-        d="flex"
-        alignItems="center"
+    <>
+      <Head>
+        <title>Jackson Hole Online Orders</title>
+      </Head>
+      <chakra.header
+        pos="fixed"
+        top="0"
+        left="0"
+        right="0"
+        zIndex="2"
+        width="full"
+        borderBottom="1px solid"
+        borderBottomColor="gray.100"
+        bg="white"
+        boxShadow="sm"
       >
-        <Link href="/" passHref>
-          <a>
-            <Flex alignItems="center">
-              <NextImage
-                src="/taxi.png"
-                alt="Logo"
-                height="50px"
-                width="50px"
-              />
+        <Flex flexDir="column">
+          <Flex bg="black">
+            <Container maxW="7xl" mx="auto" d="flex" alignItems="center">
               <Text
-                fontSize="xl"
-                fontWeight="bold"
-                mx={3}
-                display={["none", null, "block"]}
+                fontWeight="500"
+                fontSize={["xs", null, "sm"]}
+                color="white"
               >
-                JHONLINEORDER
+                tianguberabdurahman@gmail.com
+              </Text>
+            </Container>
+          </Flex>
+          <Container
+            maxW="7xl"
+            height={["3rem", null, "4rem"]}
+            mx="auto"
+            d="flex"
+            alignItems="center"
+          >
+            <Link href="/" passHref>
+              <a>
+                <Flex alignItems="center">
+                  <NextImage
+                    src="/taxi.png"
+                    alt="Logo"
+                    height="50px"
+                    width="50px"
+                  />
+                  <Text
+                    fontSize="xl"
+                    fontWeight="bold"
+                    mx={3}
+                    display={["none", null, "block"]}
+                  >
+                    JHONLINEORDER
+                  </Text>
+                </Flex>
+              </a>
+            </Link>
+            <Flex
+              flex="1"
+              alignItems="flex-start"
+              flexDir="column"
+              mx={[3, null, 10]}
+            >
+              <Text fontWeight="500" fontSize={["xs", null, "sm"]}>
+                (307) 264-8232
+              </Text>
+              <Text fontSize={["0.625rem", null, "sm"]} color="gray.500">
+                available 24 / 7
               </Text>
             </Flex>
-          </a>
-        </Link>
-        <Flex
-          flex="1"
-          alignItems="flex-start"
-          flexDir="column"
-          mx={[3, null, 10]}
-        >
-          <Text fontWeight="500" fontSize={["xs", null, "sm"]}>
-            (307) 264-8232
-          </Text>
-          <Text fontSize={["0.625rem", null, "sm"]} color="gray.500">
-            available 24 / 7
-          </Text>
+            {withCart && (
+              <>
+                <Box position="relative" onClick={onOpen}>
+                  <Bag />
+                  {items.length > 0 && (
+                    <Text
+                      as="span"
+                      position="absolute"
+                      borderRadius="50%"
+                      right="0.75rem"
+                      top="0.75rem"
+                      display="flex"
+                      alignItems="center"
+                      justifyContent="center"
+                      height="1rem"
+                      backgroundColor="black"
+                      color="white"
+                      width="1rem"
+                      fontSize="xs"
+                    >
+                      {items.length}
+                    </Text>
+                  )}
+                </Box>
+                <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
+                  <DrawerOverlay>
+                    <DrawerContent>
+                      <DrawerCloseButton />
+                      <DrawerHeader
+                        borderBottom="1px solid"
+                        borderColor="gray.200"
+                      >
+                        <Heading size="sm">My Cart</Heading>
+                      </DrawerHeader>
+                      <DrawerBody>
+                        <Cart restaurant={restaurant} />
+                      </DrawerBody>
+                    </DrawerContent>
+                  </DrawerOverlay>
+                </Drawer>
+              </>
+            )}
+          </Container>
         </Flex>
-
-        {withCart && (
-          <>
-            <Box position="relative" onClick={onOpen}>
-              <Bag />
-              {items.length > 0 && (
-                <Text
-                  as="span"
-                  position="absolute"
-                  borderRadius="50%"
-                  right="0.75rem"
-                  top="0.75rem"
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="center"
-                  height="1rem"
-                  backgroundColor="black"
-                  color="white"
-                  width="1rem"
-                  fontSize="xs"
-                >
-                  {items.length}
-                </Text>
-              )}
-            </Box>
-            <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
-              <DrawerOverlay>
-                <DrawerContent>
-                  <DrawerCloseButton />
-                  <DrawerHeader borderBottom="1px solid" borderColor="gray.200">
-                    <Heading size="sm">My Cart</Heading>
-                  </DrawerHeader>
-                  <DrawerBody>
-                    <Cart restaurant={restaurant} />
-                  </DrawerBody>
-                </DrawerContent>
-              </DrawerOverlay>
-            </Drawer>
-          </>
-        )}
-      </Container>
-    </chakra.header>
+      </chakra.header>
+    </>
   );
 };
 
